b5dd6e6390bba1fb869c011c8a8f22440541cd42
AAyaanAhmed <153097603+AAyaanAhmed@users.noreply.github.com>
2026-09-20 09:45:18 +0530

    Add files via upload

---
+++ internship_task_1.py
@@ -0,0 +1,18 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+gender = input("Enter your gender: ")
+city = input("Enter your city: ")
+country = input("Enter your country: ")
+
+print("\n"+"=" *25)
+
+print("  PERSONAL INFORMATION  ")
+
+print("=" * 25)
+print(f"Name     : {name}")
+print(f"Age      : {age}")
+print(f"Gender   : {gender}")
+print(f"City     : {city}")
+print(f"Country  : {country}")
+
+print("=" * 25)
